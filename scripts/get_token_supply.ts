830157312a899349ce4a6daa48eabf50806accf5
lance.lan <lmengsplist96@gmail.com>
2023-05-31 01:05:24 +0800

    first commit

---
+++ scripts/get_token_supply.ts
@@ -0,0 +1,79 @@
+import { ethers } from 'ethers'
+import { abiJson } from '../abi/abi'
+import { appendFileSync, readFile } from 'fs'
+
+export type BridgeData = {
+  block_height?: number
+  block_time?: number
+  PUNDIX?: string
+  USDT?: string
+  DAI?: string
+  EURS?: string
+  LINK?: string
+  C98?: string
+  WETH?: string
+  BUSD?: string
+  FRAX?: string
+  USDC?: string
+  WBTC?: string
+}
+
+const BRIDGE_ADDRESS = '0x6f1D09Fed11115d65E1071CD2109eDb300D80A27'
+const ERC20_ABI = [{
+  "constant": true,
+  "inputs": [
+    {
+      "name": "_owner",
+      "type": "address"
+    }
+  ],
+  "name": "balanceOf",
+  "outputs": [
+    {
+      "name": "balance",
+      "type": "uint256"
+    }
+  ],
+  "payable": false,
+  "stateMutability": "view",
+  "type": "function"
+}]
+
+const saveAsCSV = (data: BridgeData) => {
+  const csv = `${data.block_height},${data.block_time},${data.PUNDIX},${data.USDT},${data.DAI},${data.EURS},${data.LINK},${data.C98},${data.WETH},${data.BUSD},${data.FRAX},${data.USDC},${data.WBTC}\n`
+  try {
+    appendFileSync("./data/fx-bridge-token-supply.csv", csv)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+(async () => {
+  const provider = new ethers.JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/arkLb1R9vMhFHrhhX7hB5vCcrX5csBol')
+  const contract = new ethers.Contract(
+    BRIDGE_ADDRESS,
+    abiJson,
+    provider
+  )
+
+  const tokenList = await contract.getBridgeTokenList()
+  let res: BridgeData = {}
+  res.block_height = await provider.getBlockNumber()
+  const block = await provider.getBlock(res.block_height)
+  if (block == undefined) return
+  res.block_time = block.timestamp
+  for (const token of tokenList) {
+    const tokenAddress = token[0]
+    const tokenSymbol = token[2]
+    const tokenContract = new ethers.Contract(
+      tokenAddress,
+      ERC20_ABI,
+      provider
+    )
+    const balance = await tokenContract.balanceOf(BRIDGE_ADDRESS)
+    res[tokenSymbol as keyof typeof res] = balance.toString()
+  }
+  saveAsCSV(res)
+  return res
+})().catch(console.error).finally(() => process.exit(0))
+
